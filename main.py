464176244eefd94722416729596aca24961a747e
karabey2 <mishaustal@gmail.com>
2020-10-24 19:20:57 +0300

    обновил шаблон

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import pygame
 import random
+
 # Создаем игру и окно
 def main():
+    WIDTH = 800
+    HEIGHT = 800
+    BLACK = 0,0,0
     pygame.init()
     pygame.mixer.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("My Game")
-    clock = pygame.time.Clock()
 
     # Цикл игры
     running = True
     while running:
-        # Держим цикл на правильной скорости
-        clock.tick(FPS)
         # Ввод процесса (события)
         for event in pygame.event.get():
             # check for closing window
             if event.type == pygame.QUIT:
                 running = False
 
-        # Обновление
-
-        # Рендеринг
         screen.fill(BLACK)
         # После отрисовки всего, переворачиваем экран
         pygame.display.flip()
